4a6c248b94e040046f3489047456d79b212ae5f7
Nytheneals <nythes@gmail.com>
2018-05-27 21:29:36 -0400

    [Children Render Props] - Worked with children render props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import RenderToggleButton from "./Components/Renderprop/RenderToggleButton";
-import "./App.css";
+// import RenderToggleButton from "./Components/Renderprop/RenderToggleButton";
+import ChildrenRenderProps from "./Components/#3 ChildrenRenderProps/ChildrenRenderProps";
 
+import "./App.css";
 class App extends Component {
   render() {
     return (
@@ -9,14 +10,14 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">REACT 16.4 REVIEW</h1>
         </header>
-        <RenderToggleButton
-          render={({ on, toggle }) => (
+        <ChildrenRenderProps>
+          {({ on, toggle }) => (
             <div>
               <h1>{on && <div>It works</div>}</h1>
               <button onClick={toggle}>Click this Render prop</button>
             </div>
           )}
-        />
+        </ChildrenRenderProps>
       </div>
     );
   }
